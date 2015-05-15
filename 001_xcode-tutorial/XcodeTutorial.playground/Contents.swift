4d4593b86c7e8a938dbb5f67fb47b469ed08f831
Jan Brinkmann <lucky@the-luckyduck.de>
2015-05-15 12:20:38 +0200

    Episode #1 - Xcode Tutorial

---
+++ 001_xcode-tutorial/XcodeTutorial.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hallo von codingtutor.de"
+var neueVariable = "Noch mal Hallo"
+
+for i in 6...10 {
+    i
+}
+
---
+++ 001_xcode-tutorial/XcodeTutorial/XcodeTutorial/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  XcodeTutorial
+//
+//  Created by Jan Brinkmann on 14/05/15.
+//  Copyright (c) 2015 Jan Brinkmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var meineVariable = "hier steht inhalt"
+        println("Wir waren hier");
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
